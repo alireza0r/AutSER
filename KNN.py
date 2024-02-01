14ed3f06d2792f922785d1667115ab6d3d94825e
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-02-01 18:57:14 +0330

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,34 @@
+from sklearn.neighbors import KNeighborsClassifier
+from feature_extractor import featureExtractor
+import numpy as np
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+features, labels = featureExtractor()
+clf = KNeighborsClassifier(n_neighbors=5)
+
+KF = StratifiedKFold(n_splits=5, shuffle=True)
+for i, (x_index, y_index) in enumerate(KF.split(np.mean(features, axis=1), labels)):
+    clf.fit(np.mean(features[x_index], axis=1), labels[x_index])
+
+    print('-------------------')
+    print('Fold:', i)
+    print('Train:', clf.score(np.mean(features[x_index], axis=1), labels[x_index]))
+    print('Test:', clf.score(np.mean(features[y_index], axis=1), labels[y_index]))
+
+    disp = ConfusionMatrixDisplay.from_estimator(clf, 
+                                                 np.mean(features[x_index], axis=1), 
+                                                 labels[x_index], 
+                                                 display_labels=['H', 'A', 'S', 'N'],
+                                                 normalize='true')
+    plt.title(f'Train, Fold {i+1}')
+    plt.show()
+
+    disp = ConfusionMatrixDisplay.from_estimator(clf, 
+                                                 np.mean(features[y_index], axis=1), 
+                                                 labels[y_index],
+                                                 display_labels=['H', 'A', 'S', 'N'],
+                                                 normalize='true')
+    plt.title(f'Test, Fold {i+1}')
+    plt.show()
---
+++ feature_extractor.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+import numpy as np
+import librosa
+
+def featureExtractor(path='./AutSED_New_Name_selectedData', SR=44100):
+    files = os.listdir(path)
+    np.random.shuffle(files)
+    audio_length = int(5*SR)
+
+    label_symbol = {'H':0, 'A':1, 'S':2, 'N':3}
+    feature_stack = []
+    label_stack = []
+    for f in files:
+        label_stack.append(label_symbol[f[3]])
+        audio, SR = librosa.load(os.path.join(path, f), sr=44100)
+
+        if len(audio) < audio_length:
+            audio = np.concatenate((audio, np.zeros(audio_length - len(audio))))
+        elif len(audio) > audio_length:
+            audio = audio[:audio_length]
+
+        feature = librosa.feature.mfcc(y=audio, sr=SR, n_mfcc=20)
+        feature_stack.append(feature)
+        #print(feature.shape)
+
+    feature_stack = np.stack(feature_stack, axis=0)
+    #print(feature_stack.shape)
+    return feature_stack, np.array(label_stack)
+
